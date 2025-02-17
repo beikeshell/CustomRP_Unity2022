7854d30df677d7e35553d93e5793f188ede954c0
beikeshell <beikeshell@126.com>
2025-02-17 14:48:21 +0800

    [chapter-1]分离不透明、透明物体，添加不支持shader渲染

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using TMPro;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
@@ -18,6 +19,8 @@ public class CameraRenderer
 
     private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
+    private static Material errorMaterial;
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
@@ -30,7 +33,10 @@ public class CameraRenderer
         }
         
         Setup();
+        // 绘制视锥体内可见的物体
         DrawVisibleGeometry();
+        // 绘制不支持的Shader
+        DrawUnsupportedShaders();
         Submit();
     }
 
@@ -44,15 +50,43 @@ public class CameraRenderer
 
     private void DrawVisibleGeometry()
     {
+        // 1. 绘制不透明物体
         var sortingSettings = new SortingSettings(camera)
         {
             criteria = SortingCriteria.CommonOpaque
         };
         var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
-        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
         context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
         
+        // 2. 绘制天空盒
         context.DrawSkybox(camera);
+
+        // 3. 绘制透明物体
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    private void DrawUnsupportedShaders()
+    {
+        if (errorMaterial == null)
+        {
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
+        
+        var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
+        {
+            overrideMaterial = errorMaterial
+        };
+        for (var i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        
+        var filteringSettings = FilteringSettings.defaultValue;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
     }
 
     private void Submit()
@@ -78,4 +112,14 @@ public class CameraRenderer
 
         return false;
     }
+
+    private static ShaderTagId[] legacyShaderTagIds = new[]
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
 }
