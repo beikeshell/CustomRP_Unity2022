10ae294d1ee11c2993b65a1983d78b9abf6ee186
beikeshell <beikeshell@126.com>
2025-02-17 12:07:34 +0800

    [chapter-1]添加不透明物体渲染逻辑

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -14,10 +14,20 @@ public class CameraRenderer
 
     private CommandBuffer buffer = new CommandBuffer() {name = bufferName};
 
+    private CullingResults cullingResults;
+
+    private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
+
+        // 视锥体剔除
+        if (!Cull())
+        {
+            return;
+        }
         
         Setup();
         DrawVisibleGeometry();
@@ -27,15 +37,45 @@ public class CameraRenderer
     private void Setup()
     {
         context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
     }
 
     private void DrawVisibleGeometry()
     {
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        
         context.DrawSkybox(camera);
     }
 
     private void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out var p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+
+        return false;
     }
 }
